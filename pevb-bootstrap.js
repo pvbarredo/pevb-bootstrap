a52e3484601a59952e1d14de77c878bf2efcac65
Peter Barredo <pvbarredo@gmail.com>
2017-03-11 23:56:07 +0800

    Initialize

---
+++ pevb-bootstrap.js
@@ -0,0 +1,49 @@
+/* 
+ * pevb-bootstrap
+ *	
+ * Version: 0.0.1
+ * Author : pvbarredo@gmail.com
+ * 
+ * Description: Custom alert confirmation box made using Bootstrap
+ */
+
+
+angular.module("pevb.bootstrap",["pevb.bootstrap.alert"]);
+angular.module("pevb.bootstrap.alert",[])
+	.constant()
+	.factory('$alert', function(){
+		return {
+			confirmationBox : confirmationBox,
+			printConsole : printConsole
+		}
+
+		function confirmationBox(title, text, callbackFn, id){
+	        var modalId = id ? id : 'modalWindow';
+	        var titleText  = (title) ? 
+	                      '<div class="modal-header">' +
+	                          '<h4 class="modal-title">' + title +'</h4>' +
+	                      '</div>' : '';
+	        var messageBox = ''+
+	                '<div class="modal fade" id="'+ modalId+'" tabindex="-1" role="dialog">' +
+	                  '<div class="modal-dialog" role="document">' +
+	                    '<div class="modal-content">' +
+	                      titleText +
+	                      '<div class="modal-body">'+
+	                         text +
+	                      '</div>' +
+	                      '<div class="modal-footer">' +
+	                        '<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>' +
+	                        '<button type="button" id="confirmDialog'+modalId+'" class="btn btn-primary" data-dismiss="modal">Okay</button>' +
+	                      '</div>' +
+	                    '</div>' +
+	                  '</div>' +
+	                '</div>';
+	        if( !_.isEmpty($('#'+modalId +'')) ){
+	          $('#'+modalId +'').remove();
+	        }    
+	        $('body').append(messageBox);                
+	        $('#confirmDialog'+modalId +'').on("click",callbackFn);
+	        $('#'+modalId +'').modal('show');
+	        return false;
+	    };
+	});
